6299ddd6953bebf1fbc92a0c1656fb1da5633720
jinwoong96 <wlsdnd921@gmail.com>
2026-03-16 20:19:55 +0900

    컴포넌트 파일 생성

---
+++ src/components/EditBookInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditBookInfo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EditBookInfo;
---
+++ src/components/Navibar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navibar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Navibar;
