ac19c10fe708e321fab9f30666bb27ab662e4420
jinwoong96 <wlsdnd921@gmail.com>
2026-03-16 20:21:51 +0900

    컴포넌트 파일 생성

--- src/components/navibar.jsx
+++ src/components/navibar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const navibar = () => {
+const Navibar = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const navibar = () => {
     );
 };
 
-export default navibar;
+export default Navibar;
