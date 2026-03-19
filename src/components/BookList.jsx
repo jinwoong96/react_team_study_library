42d4349714ce01ce2231db97498bae67364eaa67
GOHOPAN <biobig112@naver.com>
2026-03-19 13:56:00 +0900

    edit booklist createbook

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -16,15 +16,11 @@ const BookList = () => {
         e.preventDefault();
         const storedBooks=JSON.parse(localStorage.getItem('books'))||[];
         const findBooks = storedBooks.filter((book)=>book.title.includes(search));
-        if(findBooks){
-            setBooks(findBooks);
-            setSearch('');
-        } else {
-            return(
-                setBooks(''),
-                <div>검색 결과가 없습니다.</div>
-            )
-        }
+        const findAuthors = storedBooks.filter((book)=>book.author.includes(search));
+        const findUsers = storedBooks.filter((book)=>book.username.includes(search));
+        const find=[...new Set([...findBooks, ...findAuthors, ...findUsers])];
+        setBooks(find);
+        setSearch('');
     }
 
     return (
@@ -32,7 +28,7 @@ const BookList = () => {
             <form className='bg-green-200 shadow-md flex justify-end gap-3 py-2' onSubmit={onSearch}>
                 <input value={search} onKeyDown={(e)=>e.key==='Enter'&&onSearch} onChange={(e)=>setSearch(e.target.value)} className='border border-gray-500 rounded py-1 px-3'></input>
                 <button type='submit' className='bg-black text-white px-3 py-1 rounded'>검색</button>
-                <div className='bg-gray-300 flex px-3 py-1 rounded ml-20 mr-10 border border-gray-400'>
+                <div className='bg-gray-300 flex px-3 py-1 rounded ml-10 mr-10 border border-gray-400'>
                     <Link to={`/create`}>책 추가</Link>
                 </div>
             </form><br />
@@ -52,7 +48,7 @@ const BookList = () => {
                         </div>
                     ))
                 ) : (
-                    <div>표시할 책이 없습니다.</div>
+                    <div id='list' className='mx-auto text-gray-500'>표시할 책이 없습니다.</div>
                 )}
             </div>
         </div>
--- src/components/CreateBook.jsx
+++ src/components/CreateBook.jsx
@@ -29,6 +29,16 @@ const CreateBook = () => {
     
     const onSubmit1=(e)=>{
         e.preventDefault();
+
+        try{
+            if(!title || !author || !content || !bookImage){
+                throw new Error("모든 정보를 입력해주세요.");
+            }
+        } catch (error) {
+            alert(error.message);
+            return;
+        }
+
         let books=JSON.parse(localStorage.getItem("books")) || [];
 
         const newBook={
@@ -38,7 +48,7 @@ const CreateBook = () => {
             username:currentUser.username,
             author,
             content,
-            writerId:currentUser.userId,
+            userid:currentUser.id,
             likes:[],
         }
         books.push(newBook);
@@ -51,14 +61,33 @@ const CreateBook = () => {
 
         navigator('/');
     }
+
+    const cancel=()=>{
+        navigator('/');
+    }
+
     return (
-        <div>
+        <div className='text-center'>
+            <h1 className='text-4xl font-bold mt-5 ml-5 mb-5'>책 추가하기</h1>
+            <hr className='mb-8 mx-auto max-w-96 h-1 bg-gray-500' />
             <form onSubmit={onSubmit1}>
-                <input placeholder='제목' onChange={(e)=>setTitle(e.target.value)}></input>
-                <input placeholder='작가' onChange={(e)=>setAuthor(e.target.value)}></input>
-                <input type='textarea' placeholder='내용' onChange={(e)=>setContent(e.target.value)}></input>
-                <input type='file' accept='image/*' onChange={handleImageChange} />
-                <button type='submit'>추가</button>
+                <input className="bg-gray-100 p-2 w-1/3 rounded-md border border-gray-500" placeholder='제목을 입력하세요.' onChange={(e)=>setTitle(e.target.value)}></input><br />
+                <input className="bg-gray-100 p-2 w-1/3 mt-2 rounded-md border border-gray-500" placeholder='작가' onChange={(e)=>setAuthor(e.target.value)}></input><br />
+                <textarea className="bg-gray-100 p-2 h-96 w-1/3 mt-2 rounded-md border border-gray-500" placeholder='내용' onChange={(e)=>setContent(e.target.value)}></textarea><br />
+                
+                <div class="flex items-center justify-center mx-auto rounded-md border border-gray-500 bg-gray-100 w-1/4 h-40">
+                    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded-base cursor-pointer hover:bg-neutral-tertiary-medium">
+                        <div class="flex flex-col items-center justify-center text-body pt-5 pb-6">
+                            <svg class="w-8 h-8 mt-5 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2"/></svg>
+                            <p class="mb-2 text-md text-gray-700">이미지 업로드</p>
+                            <p class="text-xs text-gray-500">클릭 또는 드래그하여 파일 선택</p>
+                        </div>
+                        <input id='dropzone-file' type='file' accept='image/*' class='hidden' onChange={handleImageChange} /><br />
+                    </label>
+                </div> 
+                
+                <button type='submit' className='text-lg bg-blue-500 text-white px-6 py-1 rounded mt-5 mr-2 border border-gray-400'>추가</button>
+                <button type='button' className='text-lg bg-red-500 text-white px-6 py-1 rounded mt-5 ml-2 border border-gray-400'onClick={cancel}>취소</button>
             </form>
         </div>
     );
