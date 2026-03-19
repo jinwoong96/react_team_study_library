e64b6237336b9fda6e7707fece15a557a1649047
jinwoong96 <158314558+jinwoong96@users.noreply.github.com>
2026-03-19 10:35:06 +0900

    Update Navibar.jsx

--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from "./AuthContextPro";
+import AdminPage from './AdminPage';
 
 const Navibar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { currentUser, logout } = useAuth();
+  const navigator = useNavigate();
+
+  useEffect(()=>{
+    if(!currentUser){
+      navigator('/login');
+    }
+  },[currentUser]);
+  
+  return (
+    <nav style={{
+      display: 'flex',
+      padding: '10px'
+    }}>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '5px'
+      }}>
+        <Link to="/">홈</Link>
+
+        <AdminPage />
+      </div>
+
+      <div style={{
+        marginLeft: 'auto',
+        display: 'flex',
+        gap: '10px'
+      }}>
+        {currentUser ? (
+          <>
+            <span>{currentUser.username}님</span>
+            <button onClick={logout}>로그아웃</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navibar;
